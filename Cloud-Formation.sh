10465fea00c4653ca5272c7b725a7e4b7f2c01dd
ctproject4 <36787361+ctproject4@users.noreply.github.com>
2018-10-01 14:51:48 +0530

    Update Cloud-Formation.sh

--- Cloud-Formation.sh
+++ Cloud-Formation.sh
@@ -11,8 +11,4 @@ cd ~/.kube
 curl -O https://amazon-eks.s3-us-west-2.amazonaws.com/cloudformation/2018-08-30/aws-auth-cm.yaml
 echo $NodeInstanceRole
 sed -i "s#<ARN of instance role (not instance profile)>#$NodeInstanceRole#" aws-auth-cm.yaml
-cd /home/ubuntu
-sudo chown ubuntu:ubuntu .aws
-cd .aws && sudo chown ubuntu:ubuntu config credentials
-
 kubectl apply -f aws-auth-cm.yaml
