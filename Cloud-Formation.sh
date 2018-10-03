ff76e266a01b9ab9da4d60343276f1142ca20492
ctproject4 <36787361+ctproject4@users.noreply.github.com>
2018-10-03 16:09:51 +0530

    Update Cloud-Formation.sh

--- Cloud-Formation.sh
+++ Cloud-Formation.sh
@@ -5,12 +5,13 @@ cp /home/ubuntu/sampletemplate.yaml .
 cp /home/ubuntu/rdp .
 aws cloudformation create-stack --stack-name devops-demo-worker-nodes --template-body file://sampletemplate.yaml --capabilities CAPABILITY_IAM
 sleep 5m
-NodeInstanceRole=$(aws cloudformation describe-stacks --stack-name devops-demo-worker-nodes | grep OutputValue | cut -d '"' -f4)
+#NodeInstanceRole=$(aws cloudformation describe-stacks --stack-name devops-demo-worker-nodes | grep OutputValue | cut -d '"' -f4)
 
-cd ~/.kube
-curl -O https://amazon-eks.s3-us-west-2.amazonaws.com/cloudformation/2018-08-30/aws-auth-cm.yaml
-echo $NodeInstanceRole
-sed -i "s#<ARN of instance role (not instance profile)>#$NodeInstanceRole#" aws-auth-cm.yaml
+#cd ~/.kube
+#curl -O https://amazon-eks.s3-us-west-2.amazonaws.com/cloudformation/2018-08-30/aws-auth-cm.yaml
+#echo $NodeInstanceRole
+#sed -i "s#<ARN of instance role (not instance profile)>#$NodeInstanceRole#" aws-auth-cm.yaml
 pwd
-kubectl apply -f ~/.kube/aws-auth-cm.yaml
+#kubectl apply -f ~/.kube/aws-auth-cm.yaml
 pwd
+whoami
