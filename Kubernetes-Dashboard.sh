8aefaa6acbcb1b33f0d0e53d72b5ee8c7a0b6dbf
ctproject4 <36787361+ctproject4@users.noreply.github.com>
2018-10-04 12:15:19 +0530

    Update Kubernetes-Dashboard.sh

--- Kubernetes-Dashboard.sh
+++ Kubernetes-Dashboard.sh
@@ -4,3 +4,8 @@ sudo apt-get update -y
 sudo apt-get install lxde -y
 sudo start lxdm
 sudo apt-get install xrdp -y
+
+KUBECONFIG=$KUBECONFIG:~/.kube/config-devops-demo
+export KUBECONFIG
+echo 'export KUBECONFIG=$KUBECONFIG:~/.kube/config-devops-demo' >> ~/.bashrc
+
