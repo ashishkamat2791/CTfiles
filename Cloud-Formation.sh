f6fb0859573651cf5476b9be7be0364c77b558e1
ctproject4 <36787361+ctproject4@users.noreply.github.com>
2018-10-07 00:58:51 +0530

    Update Cloud-Formation.sh

--- Cloud-Formation.sh
+++ Cloud-Formation.sh
@@ -2,7 +2,7 @@
 ############################################CLOUD FORMATION(STACK)###############################################
 cd ~
 cp /home/ubuntu/sampletemplate.yaml .
-cp /home/ubuntu/rdp .
+cp /home/ubuntu/Kubernetes-Dashboard.sh .
 aws cloudformation create-stack --stack-name devops-demo-worker-nodes --template-body file://sampletemplate.yaml --capabilities CAPABILITY_IAM
 sleep 5m
 NodeInstanceRole=$(aws cloudformation describe-stacks --stack-name devops-demo-worker-nodes | grep OutputValue | cut -d '"' -f4)
@@ -15,4 +15,3 @@ KUBECONFIG=$KUBECONFIG:~/.kube/config-devops-demo
 export KUBECONFIG
 echo 'export KUBECONFIG=$KUBECONFIG:~/.kube/config-devops-demo' >> ~/.bashrc
 kubectl apply -f ~/.kube/aws-auth-cm.yaml
-
