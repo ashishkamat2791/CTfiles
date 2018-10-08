0b3882e6de052fda7a98b928ba3d1c879a0f7f41
ctproject4 <36787361+ctproject4@users.noreply.github.com>
2018-10-08 19:23:42 +0530

    Update Cloud-Formation.sh

--- Cloud-Formation.sh
+++ Cloud-Formation.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 ############################################CLOUD FORMATION(STACK)###############################################
+
+CLUSTER_NAME=`grep clusterName properties | cut -d "=" -f2`
+
 cd ~
 cp /home/ubuntu/sampletemplate.yaml .
 cp /home/ubuntu/Kubernetes-Dashboard.sh .
-aws cloudformation create-stack --stack-name devops-demo-worker-nodes --template-body file://sampletemplate.yaml --capabilities CAPABILITY_IAM
+aws cloudformation create-stack --stack-name $CLUSTER_NAME-worker-nodes --template-body file://sampletemplate.yaml --capabilities CAPABILITY_IAM
 sleep 5m
-NodeInstanceRole=$(aws cloudformation describe-stacks --stack-name devops-demo-worker-nodes | grep OutputValue | cut -d '"' -f4)
+NodeInstanceRole=$(aws cloudformation describe-stacks --stack-name $CLUSTER_NAME-worker-nodes | grep OutputValue | cut -d '"' -f4)
 
 cd ~/.kube
 curl -O https://amazon-eks.s3-us-west-2.amazonaws.com/cloudformation/2018-08-30/aws-auth-cm.yaml
