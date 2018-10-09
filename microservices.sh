45c284fd70c130d5d6c4f808c39b534969db4120
ctproject4 <36787361+ctproject4@users.noreply.github.com>
2018-10-09 16:57:46 +0530

    Update microservices.sh

--- microservices.sh
+++ microservices.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CLUSTER_NAME=`grep clusterName properties | cut -d "=" -f2`
+CLUSTER_NAME=`grep cluster_Name properties | cut -d "=" -f2`
 
 cd /home/ubuntu/
 KUBECONFIG=$KUBECONFIG:~/.kube/config-$CLUSTER_NAME
